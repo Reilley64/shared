14c071d0ec77b65b311dddd4aac6aca6f4773e87
Reilley Gray <reilleygray@gmail.com>
2020-11-12 22:25:49 +1100

    Move back to spring data jpa

--- src/main/java/me/reilley/shared/domain/models/Model.java
+++ src/main/java/me/reilley/shared/domain/models/Model.java
@@ -5,16 +5,22 @@ import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
 import java.util.Date;
 import java.util.UUID;
 
 @Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Model {
     @JsonIgnore
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private UUID uuid = UUID.randomUUID();
 
     @CreatedDate
--- src/main/java/me/reilley/shared/domain/repositories/Repository.java
+++ src/main/java/me/reilley/shared/domain/repositories/Repository.java
@@ -1,14 +1,14 @@
 package me.reilley.shared.domain.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.io.Serializable;
 import java.util.Optional;
 import java.util.UUID;
 
 @NoRepositoryBean
-public interface Repository<T, ID extends Serializable> extends PagingAndSortingRepository<T, ID> {
+public interface Repository<T, ID extends Serializable> extends JpaRepository<T, ID> {
     Optional<T> findByUuid(UUID uuid);
 
     boolean existsByUuid(UUID uuid);
