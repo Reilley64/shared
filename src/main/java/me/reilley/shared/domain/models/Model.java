534fc6bf8332ad89bdcd37a6e990fc5901de2fad
Reilley Gray <reilleygray@gmail.com>
2020-11-12 22:36:25 +1100

    Fix id problem

--- src/main/java/me/reilley/shared/domain/models/Model.java
+++ src/main/java/me/reilley/shared/domain/models/Model.java
@@ -3,7 +3,6 @@ package me.reilley.shared.domain.models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
